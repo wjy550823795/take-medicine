46ef1d1a8bb24988158ca656ba34925a39e48026
wangjy76 <wangjy76@lenovo.com>
2022-07-24 11:08:53 +0800

    自增策略

--- src/main/java/com/tencent/wxcloudrun/model/User.java
+++ src/main/java/com/tencent/wxcloudrun/model/User.java
@@ -1,11 +1,14 @@
 package com.tencent.wxcloudrun.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import lombok.Data;
 
 @Data
 public class User implements Serializable {
 
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
     private String name;
