f45e12dd12a9e1ae1206ec432aa2c3d2fba1d7a5
wangjy76 <wangjy76@lenovo.com>
2022-07-24 13:17:56 +0800

    token会失效？

--- src/main/java/com/tencent/wxcloudrun/service/impl/RobotTokenComponent.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/RobotTokenComponent.java
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @Component
 public class RobotTokenComponent {
+
     /**
      * 缓存的access_token
      */
@@ -24,32 +25,41 @@ public class RobotTokenComponent {
 
 
     public String getAccessToken() {
+        long nowMillions = System.currentTimeMillis();
+        boolean flag = expiresTime < nowMillions;
+        log.info("expiresTime:{},nowMillions:{},flag:{}", expiresTime, nowMillions, flag);
         // 判断accessToken是否已经过期，如果过期需要重新获取
-        if (accessToken == null || expiresTime < System.currentTimeMillis()) {
-            RestTemplate restTemplate = RestTemplateUtil.getInstance();
-            Map<String, String> params = new HashMap<>(2);
-            String appId = "wx45657c6db53f14c5";
-            String appSecret = "841f5d9f0418ee4da9a007f123effaec";
-            params.put("APPID", appId);
-            params.put("APPSECRET", appSecret);
-            ResponseEntity<String> responseEntity = restTemplate.getForEntity(
-                "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}",
-                String.class, params);
-            String body = responseEntity.getBody();
-            JSONObject object = JSON.parseObject(body);
-            Integer errcode = object.getInteger("errcode");
-            if (errcode != null && errcode != 0) {
-                String errmsg = object.getString("errmsg");
-                log.info("请求accessToken失败，返回码：" + errcode + "，错误信息：" + errmsg);
-                // 抛个异常
-            }
-            // 缓存accessToken
-            accessToken = object.getString("access_token");
-            // 设置accessToken的失效时间
-            long expiresIn = object.getLong("expires_in");
-            // 失效时间 = 当前时间 + 有效期(提前一分钟，也可不提前，这里只是稳妥一下)
-            expiresTime = System.currentTimeMillis() + (expiresIn - 60) * 1000;
+        if (accessToken == null || flag) {
+            forceRefreshToken(nowMillions);
         }
+        return accessToken;
+    }
+
+    public String forceRefreshToken(long nowMillions) {
+        RestTemplate restTemplate = RestTemplateUtil.getInstance();
+        Map<String, String> params = new HashMap<>(2);
+        String appId = "wx45657c6db53f14c5";
+        String appSecret = "841f5d9f0418ee4da9a007f123effaec";
+        params.put("APPID", appId);
+        params.put("APPSECRET", appSecret);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+            "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}",
+            String.class, params);
+        String body = responseEntity.getBody();
+        JSONObject object = JSON.parseObject(body);
+        log.info("getAccessToken object:{}", object.toJSONString());
+        Integer errcode = object.getInteger("errcode");
+        if (errcode != null && errcode != 0) {
+            String errmsg = object.getString("errmsg");
+            log.info("请求accessToken失败，返回码：" + errcode + "，错误信息：" + errmsg);
+            // 抛个异常
+        }
+        // 缓存accessToken
+        accessToken = object.getString("access_token");
+        // 设置accessToken的失效时间
+        long expiresIn = object.getLong("expires_in");
+        // 失效时间 = 当前时间 + 有效期(提前一分钟，也可不提前，这里只是稳妥一下)
+        expiresTime = nowMillions + (expiresIn - 60) * 1000;
         return accessToken;
     }
 }
--- src/main/java/com/tencent/wxcloudrun/service/impl/SendService.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/SendService.java
@@ -1,6 +1,7 @@
 package com.tencent.wxcloudrun.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.tencent.wxcloudrun.dto.WxMsgDto;
 import com.tencent.wxcloudrun.dto.WxTemplateDataDto;
 import com.tencent.wxcloudrun.model.User;
@@ -46,10 +47,10 @@ public class SendService {
      */
     public String sendVlogCompleteTemplateMsg(String openid) {
         RestTemplate restTemplate = RestTemplateUtil.getInstance();
+        String accessToken = robotTokenComponent.getAccessToken();
         //发送订阅消息的url，官网地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/subscribe-message/subscribeMessage.send.html
         String url =
-            "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token="
-                + robotTokenComponent.getAccessToken();
+            "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=";
         //拼接推送的模版
         WxMsgDto wxMsgDto = new WxMsgDto();
         //用户的openid（要发送给的那个用户）
@@ -68,9 +69,16 @@ public class SendService {
         map.put("phrase3", new WxTemplateDataDto("未吃药"));
         map.put("thing5", new WxTemplateDataDto("王馋馋"));
         wxMsgDto.setData(map);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, wxMsgDto, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url + accessToken, wxMsgDto, String.class);
         String body = responseEntity.getBody();
         log.info("sendVlogCompleteTemplateMsg:{}", body);
+        JSONObject object = JSON.parseObject(body);
+        if (object.containsKey("errcode") && object.getInteger("errcode").equals(40001)) {
+            body = restTemplate.postForEntity(
+                    url + robotTokenComponent.forceRefreshToken(System.currentTimeMillis()), wxMsgDto, String.class)
+                .getBody();
+            log.info("sendVlogCompleteTemplateMsg force refresh:{}", body);
+        }
         return body;
     }
 
