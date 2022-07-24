7267f38ec01b1ef471c01188690678ac76ccc9a6
wangjy76 <wangjy76@lenovo.com>
2022-07-24 11:51:29 +0800

    指定table名称

--- src/main/java/com/tencent/wxcloudrun/model/User.java
+++ src/main/java/com/tencent/wxcloudrun/model/User.java
@@ -2,10 +2,12 @@ package com.tencent.wxcloudrun.model;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import lombok.Data;
 
 @Data
+@TableName("User")
 public class User implements Serializable {
 
     @TableId(type = IdType.AUTO)
