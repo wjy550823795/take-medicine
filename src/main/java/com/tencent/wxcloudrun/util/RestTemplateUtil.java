4d12b4ca6b59dc3d6d128a52e3b1be8c6946bed9
wangjy76 <wangjy76@lenovo.com>
2022-08-06 19:39:58 +0800

    1

--- src/main/java/com/tencent/wxcloudrun/util/RestTemplateUtil.java
+++ src/main/java/com/tencent/wxcloudrun/util/RestTemplateUtil.java
@@ -1,12 +1,15 @@
 package com.tencent.wxcloudrun.util;
 
-import com.tencent.wxcloudrun.config.WxMappingJackson2HttpMessageConverter;
+import java.nio.charset.StandardCharsets;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 public class RestTemplateUtil {
+
     public static RestTemplate getInstance() {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        //restTemplate.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
         return restTemplate;
     }
 }
